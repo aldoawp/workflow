915ea3fd6483fa150c0c7c24e2ff83a6edea1af0
awibowo31_FMI <awibowo31@fmi.com>
2026-05-17 23:52:01 +0800

    feat: copy .agents and templates/docs to project root on install

---
+++ postinstall.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const path = require('path');
+
+const src = __dirname;
+const dest = process.env.INIT_CWD || process.cwd();
+
+// Skip if installing the package itself
+if (src === dest) process.exit(0);
+
+function copyDir(from, to) {
+  fs.mkdirSync(to, { recursive: true });
+  for (const entry of fs.readdirSync(from, { withFileTypes: true })) {
+    const srcPath = path.join(from, entry.name);
+    const destPath = path.join(to, entry.name);
+    if (entry.isDirectory()) {
+      copyDir(srcPath, destPath);
+    } else {
+      fs.copyFileSync(srcPath, destPath);
+    }
+  }
+}
+
+const dirs = ['.agents', path.join('templates', 'docs')];
+
+for (const dir of dirs) {
+  const from = path.join(src, dir);
+  const to = path.join(dest, dir);
+  if (fs.existsSync(from)) {
+    copyDir(from, to);
+    console.log(`  copied: ${dir}`);
+  }
+}
+
+console.log('workflow installed to', dest);
